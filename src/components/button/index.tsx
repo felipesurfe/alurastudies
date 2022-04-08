ecdf3eaa187a94bb7fc27480c9ba1fffc396686c
felipesurfe <felipe_surfe@hotmail.com>
2022-04-07 21:55:28 -0300

    add button children

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -5,7 +5,7 @@ class Button extends React.Component <{ texto: string }>{
     render() {
         return (
             <button className={style.botao}>
-                {this.props.texto}
+                {this.props.children}
             </button>
         )
     }
--- src/components/formulario/index.tsx
+++ src/components/formulario/index.tsx
@@ -32,9 +32,9 @@ class Formulario extends React.Component {
                         required
                     />
                 </div>
-                <Button 
-                    texto="Adicionar"
-                />
+                <Button> 
+                Adicionar
+                </Button>
             </form>
         )
     }
